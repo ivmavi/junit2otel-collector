6a6c59ca226ac805e1905510e56ee7d550a20ab4
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-11-12 12:51:01 +0100

    feat: prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # junit2otel-collector
+
 GitHub Action to process and send to a OpenTelemetry service a JUnit result file stored as artifact on a workflow run.
--- script.test.js
+++ script.test.js
@@ -1,38 +1,38 @@
 const github = require("@actions/github");
 const script = require("./script");
-const code = require('@actions/core');
+const code = require("@actions/core");
 const { coerce } = require("yargs");
 const debug = code.debug;
 
 test("downloadReport", async () => {
-    const { GITHUB_TOKEN } = process.env;
+  const { GITHUB_TOKEN } = process.env;
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-    owner = "ivmavi";
-    repo = "tests-python";
-    workflow_id = ".github/workflows/test.yml";
-    reportName = "junit-test-results.xml";
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+  owner = "ivmavi";
+  repo = "tests-python";
+  workflow_id = ".github/workflows/test.yml";
+  reportName = "junit-test-results.xml";
 
-    const runs = await octokit.rest.actions.listWorkflowRuns({
-      owner,
-      repo,
-      workflow_id,
-    });
+  const runs = await octokit.rest.actions.listWorkflowRuns({
+    owner,
+    repo,
+    workflow_id,
+  });
 
-    workflowRunId = runs.data.workflow_runs[0].id;;
+  workflowRunId = runs.data.workflow_runs[0].id;
 
-    report = {
-      github: octokit,
-      owner,
-      repo,
-      workflowRunId,
-      reportName,
-    };
+  report = {
+    github: octokit,
+    owner,
+    repo,
+    workflowRunId,
+    reportName,
+  };
 
-    core.setOutput('owner', owner);
-    core.setOutput('repo', repo);
-    core.setOutput('workflowRunId', workflowRunId);
-    core.setOutput('reportName', reportName);
+  core.setOutput("owner", owner);
+  core.setOutput("repo", repo);
+  core.setOutput("workflowRunId", workflowRunId);
+  core.setOutput("reportName", reportName);
 
-    await script.downloadReport(report);
+  await script.downloadReport(report);
 });
